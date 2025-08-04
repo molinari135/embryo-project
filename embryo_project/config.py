7c66bb63a9f775c572594949cd3ee5059a8efc49
Ester <e.molinari3@studenti.uniba.it>
2025-08-04 14:54:27 +0200

    Add notebook for annotations preprocessing

--- embryo_project/config.py
+++ embryo_project/config.py
@@ -21,6 +21,9 @@ MODELS_DIR = PROJ_ROOT / "models"
 REPORTS_DIR = PROJ_ROOT / "reports"
 FIGURES_DIR = REPORTS_DIR / "figures"
 
+# This file should contain all the labels for the images
+ANNOTATIONS_FILE = RAW_DATA_DIR / "annotations.xlsx"
+
 # If tqdm is installed, configure loguru with tqdm.write
 # https://github.com/Delgan/loguru/issues/135
 try:
